950ff75437c8e1b65e38cb2b1fcc9eb91bcefdac
DevMekus <nnaji.nnaemeka712@gmail.com>
2023-09-07 06:43:08 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Will use a printf statement to display value
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
