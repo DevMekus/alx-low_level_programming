a2ab8115096cd03e0edb9097f0938c1b88cc45aa
DevMekus <nnaji.nnaemeka712@gmail.com>
2023-09-07 06:33:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Main is our entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
