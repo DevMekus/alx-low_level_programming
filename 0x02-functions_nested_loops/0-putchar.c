2f23cb56b8d1fbcdf505403a94798bde76820712
DevMekus <nnaji.nnaemeka712@gmail.com>
2023-09-12 13:17:02 +0100

    0-putchar.c task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <unistd.h>
-/**
- * main - We will print using _putchar
- * Return: 0 (success)
- **/
-int main(void){
-int _putchar(char c)
-{
-return (write(1, &c, 1));
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
+
+
+int main(void)	{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
-return(0);
-}
+
